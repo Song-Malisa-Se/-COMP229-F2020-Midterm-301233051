5952e1bcb54185f5947dc068278954ba7641f1b4
Song Malisa Se <sesongmaliisa@gmail.com>
2022-10-29 19:57:50 -0400

    after changing to  mongo URI

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://127.0.0.1/books229"
+  //"URI": "mongodb://127.0.0.1/books229"
+  "URI": "mongodb+srv://malisa:LG3qhzGPPeVWbbgX@mongodbservermidterm.2mmglre.mongodb.net/books229?retryWrites=true&w=majority"
 };
